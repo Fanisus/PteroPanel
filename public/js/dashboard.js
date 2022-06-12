eb0fd9312d0226f10fafcfb5b0b3fec262d81286
Fanisus <09rfanisus@gmail.com>
2022-06-12 20:12:00 +0530

    Final

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,5 +1,11 @@
 let adminVisiblilities = []
+let sidebar_size_toggle_state = "large"
 function sidebar_size_toggle() {
     console.log("cliecks");
-    
+    if (sidebar_size_toggle_state === "large") {
+        
+    }
+    else if (sidebar_size_toggle_state === "small") {
+
+    }
 }
