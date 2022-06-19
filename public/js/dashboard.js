9dffd112c4793163fa3e77df4ea43e1b1c703978
Fanisus <09rfanisus@gmail.com>
2022-06-19 15:48:25 +0530

    Just a commit

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -6,25 +6,19 @@ function sidebar_size_toggle() {
     console.log("cliecks");
     if (sidebar_size_toggle_state === "large") {
         sidebar_size_toggle_state = "small";
-        // document.getElementById('sidebar').classList.add('sidebar-small')
-        // document.getElementById('sidebar').classList.remove('sidebar-large')
-
         document.documentElement.style.setProperty('--current-sidebar-width', getComputedStyle(document.body).getPropertyValue('--sidebar-small-width')) //10vw
-        // document.getElementById('icons').classList.remove('inline-block')
-        // document.getElementById('icons').classList.add('hide')
         for (var i = 0; i < document.getElementsByClassName('icon-only').length; i++) {
             document.getElementsByClassName('icon-only').item(i).classList.add('hide')
         }
+        document.getElementById("sidebar-button").style.transform = "rotate(90deg)"
     }
     else if (sidebar_size_toggle_state === "small") {
         sidebar_size_toggle_state = "large";
-        // document.getElementById('sidebar').classList.add('sidebar-large')
-        // document.getElementById('sidebar').classList.remove('sidebar-small')
+        document.documentElement.style.setProperty('--current-sidebar-width', getComputedStyle(document.body).getPropertyValue('--sidebar-large-width'))
         for (let i = 0; i < document.getElementsByClassName('icon-only').length; i++) {
             document.getElementsByClassName('icon-only').item(i).classList.remove('hide')
         }
-
-        document.documentElement.style.setProperty('--current-sidebar-width', getComputedStyle(document.body).getPropertyValue('--sidebar-large-width'))
+        document.getElementById("sidebar-button").style.transform = "rotate(270deg)"
     }
 }
 
--- public/js/servers.js
+++ public/js/servers.js
@@ -1,11 +1,24 @@
 let adminVisiblilities = []
 let sidebar_size_toggle_state = "large"
+console.log(document.getElementsByClassName('icon-only'));
+document.documentElement.style.setProperty('--current-sidebar-width', getComputedStyle(document.body).getPropertyValue('--sidebar-large-width'))
 function sidebar_size_toggle() {
     console.log("cliecks");
     if (sidebar_size_toggle_state === "large") {
-        
+        sidebar_size_toggle_state = "small";
+        document.documentElement.style.setProperty('--current-sidebar-width', getComputedStyle(document.body).getPropertyValue('--sidebar-small-width')) //10vw
+        for (var i = 0; i < document.getElementsByClassName('icon-only').length; i++) {
+            document.getElementsByClassName('icon-only').item(i).classList.add('hide')
+        }
     }
     else if (sidebar_size_toggle_state === "small") {
+        sidebar_size_toggle_state = "large";
+        for (let i = 0; i < document.getElementsByClassName('icon-only').length; i++) {
+            document.getElementsByClassName('icon-only').item(i).classList.remove('hide')
+        }
 
+        document.documentElement.style.setProperty('--current-sidebar-width', getComputedStyle(document.body).getPropertyValue('--sidebar-large-width'))
     }
 }
+
+console.log(window.location.pathname);
